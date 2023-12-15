ee3061fd8f7d383d32314c9d239e89a0683cbf21
CataeroGong <cataerogong@hotmail.com>
2023-12-15 16:54:25 +0800

    v0.1.1 wip

---
+++ HISTORY.md
@@ -0,0 +1,11 @@
+## v0.1.1
+
+* [!] bugfix: 运行 `strapp.exe -h` 报错
+
+  PyInstaller 打包成 exe 时使用 `-w` 参数打包成图形界面程序，用 pythonw.exe 运行，没有 console 窗口可供输出 help 信息，导致报错。
+
+  改为显示一个帮助网页。
+
+## v0.1.0
+
+  第一个可用的版本
--- strapp.py
+++ strapp.py
@@ -8,7 +8,7 @@ from webui import webui
 
 from utils import open_any_enc
 
-APP_VER = '0.1.0'
+APP_VER = '0.1.1'
 
 CUR_DIR = os.path.abspath(os.getcwd())
 APP_NAME = os.path.splitext(os.path.basename(sys.argv[0]))[0]
@@ -23,10 +23,12 @@ def is_port_valid(p: int):
 	finally:
 		s.close()
 
-def main():
+def show_msg_win(win: webui.window, msg: str, browser):
+	win.show(f'<html><head><meta charset="UTF-8" /><title>{APP_NAME} v{APP_VER}</title></head><body style="background:#fdf3df;color:#111111;">{msg}<script src="/webui.js"></script></body></html>', browser)
 
+def main():
 	# 读取命令行参数
-	parser = argparse.ArgumentParser()
+	parser = argparse.ArgumentParser(description='STRapp - 让 SimpleTextReader 像本地应用程序一样运行', add_help=False)
 	parser.add_argument('-r', '--webroot', default='', help='网页根目录（支持相对路径或绝对路径）[默认: 当前目录]')
 	parser.add_argument('-s', '--startpage', metavar='HTML_FILE', default='index.html', help='应用主页 [默认: index.html]')
 	parser.add_argument('--js', metavar='JS_FILE', help='加载后执行 js 脚本文件（支持相对路径或绝对路径）')
@@ -34,12 +36,15 @@ def main():
 	parser.add_argument('-b', '--browser', default='any', help='使用的浏览器（必须是本机已安装的）[默认: 系统缺省浏览器][可选项: chrome,firefox,edge,safari,chromium,opera,brave,vivaldi,epic,yandex]')
 	parser.add_argument('--width', default=0, help='窗口宽度 [默认: 上次运行时宽度]')
 	parser.add_argument('--height', default=0, help='窗口高度 [默认: 上次运行时高度]')
+	parser.add_argument('-h', '--help', action='store_true', help='显示帮助信息')
 	parser.epilog = '【*注意*】如果网页会用到浏览器存储（如cookie,LocalStorage,indexdb），则必须指定端口(-p,--port)'
 	cfg = parser.parse_args()
 	print('args:', cfg)
+	help_msg = f'<pre style="font-size:1rem;color:gray">{parser.format_help()}</pre>'
 
 	WEB_ROOT = os.path.normpath(os.path.join(CUR_DIR, cfg.webroot))
 	START_PAGE = os.path.normpath(os.path.join(WEB_ROOT, cfg.startpage))
+	BROWSER = webui.browser.__dict__.get(cfg.browser, webui.browser.any)
 
 	# Create a window object
 	MyWindow = webui.window()
@@ -47,16 +52,18 @@ def main():
 	MyWindow.set_size(cfg.width, cfg.height)
 	MyWindow.set_root_folder(WEB_ROOT)
 
-	if not is_port_valid(cfg.port):
-		MyWindow.show(f'<html><head><meta charset="UTF-8"></head><body><h1>端口已被占用！</h1>PORT: ({cfg.port})<script src="/webui.js"></script></body></html>')
+	if (cfg.help):
+		show_msg_win(MyWindow, help_msg, BROWSER)
+	elif not is_port_valid(cfg.port):
+		show_msg_win(MyWindow, f'<h1>端口已被占用！</h1>PORT: ({cfg.port})<hr>{help_msg}', BROWSER)
 	elif not os.path.exists(START_PAGE):
-		MyWindow.show(f'<html><head><meta charset="UTF-8"></head><body><h1>应用主页文件不存在！</h1>START_PAGE: ({START_PAGE})<script src="/webui.js"></script></body></html>')
+		show_msg_win(MyWindow, f'<h1>应用主页文件不存在！</h1>START_PAGE: ({START_PAGE})<hr>{help_msg}', BROWSER)
 	else:
 		# 在页面末尾增加载入 webui.js，关闭窗口时才能结束进程
 		with open_any_enc(START_PAGE) as f:
 			html = str(f.read())
 			html = html.replace('</html>', '<script src="/webui.js"></script></html>')
-			MyWindow.show(html, webui.browser.__dict__.get(cfg.browser, webui.browser.any))
+			MyWindow.show(html, BROWSER)
 		# print('js-file:', cfg.js)
 		if (cfg.js):
 			js_file = os.path.normpath(os.path.join(CUR_DIR, cfg.js))
